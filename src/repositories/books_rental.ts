a2c4b8910d48b790d485ee2342eab079bd2a8728
David Wesley <contato.dwesley@outlook.com>
2024-03-07 22:16:30 -0300

    feat: implement some bookrentals repository logics

--- src/repositories/books_rental.ts
+++ src/repositories/books_rental.ts
@@ -1,10 +1,9 @@
 import { db, schema } from "@/db/db.ts"
-import { Book } from "@/interfaces/models/books.ts"
 import { BooksRental, NewBooksRental } from "@/interfaces/models/booksRental.ts"
-import { User } from "@/interfaces/models/users.ts"
 import { IBooksRentalRepository } from "@/interfaces/repositories/booksRental.ts"
 import { BooksRepository } from "@/repositories/books.ts"
 import { UsersRepository } from "@/repositories/users.ts"
+import { eq } from "drizzle-orm"
 
 export class BooksRentalRepository implements IBooksRentalRepository {
     protected static convert(dbBookRental: typeof schema.booksRental.$inferSelect): BooksRental {
@@ -21,7 +20,7 @@ export class BooksRentalRepository implements IBooksRentalRepository {
 
     protected static convertWith(
         dbBookRental: typeof schema.booksRental.$inferSelect & { user: typeof schema.users.$inferSelect; book: typeof schema.books.$inferSelect }
-    ): BooksRental & { user: User; book: Book } {
+    ): Required<BooksRental> {
         const data: Required<BooksRental> = {
             book_id: dbBookRental.book_id,
             id: dbBookRental.id,
@@ -52,7 +51,7 @@ export class BooksRentalRepository implements IBooksRentalRepository {
         return BooksRentalRepository.convert(createdBookRental)
     }
 
-    public async getById(id: BooksRental["id"]): Promise<(BooksRental & { user: User; book: Book }) | undefined> {
+    public async getById(id: BooksRental["id"]): Promise<Required<BooksRental> | undefined> {
         const bookRental = await db.query.booksRental.findFirst({
             where: (table, { eq }) => eq(table.id, id),
             with: {
@@ -65,59 +64,25 @@ export class BooksRentalRepository implements IBooksRentalRepository {
         return BooksRentalRepository.convertWith(bookRental)
     }
 
-    // TODO:
-    public async getByBookId(book_id: string): Promise<BooksRental | undefined> {
-        // const bookRental = await BooksRentalModel.findOne({
-        //     where: { book_id },
-        //     include: [
-        //         {
-        //             model: UsersModel,
-        //             as: "booksRentalModelUsers",
-        //         },
-        //         {
-        //             model: BooksModel,
-        //             as: "booksRentalModelBooks",
-        //         },
-        //     ],
-        // })
-        // if (!bookRental) return undefined
-        // return {
-        //     id: bookRental.id,
-        //     book_id: bookRental.book_id,
-        //     book: bookRental.booksRentalModelBooks,
-        //     user_id: bookRental.user_id,
-        //     user: bookRental.booksRentalModelUsers,
-        //     rental_time: bookRental.rental_time,
-        //     rented_at: bookRental.rented_at,
-        // }
+    public async getByBookId(book_id: BooksRental["book_id"]): Promise<Required<BooksRental> | undefined> {
+        const bookRental = await db.query.booksRental.findFirst({
+            where: (table, { eq }) => eq(table.book_id, book_id),
+            with: {
+                book: true,
+                user: true,
+            },
+        })
+
+        if (!bookRental) return undefined
+        return BooksRentalRepository.convertWith(bookRental)
     }
 
-    // TODO:
     public async listAll(): Promise<BooksRental[]> {
-        //     const booksRentals = await BooksRentalModel.findAll({
-        //         include: [
-        //             {
-        //                 model: UsersModel,
-        //                 as: "booksRentalModelUsers",
-        //             },
-        //             {
-        //                 model: BooksModel,
-        //                 as: "booksRentalModelBooks",
-        //             },
-        //         ],
-        //     })
-        //     return booksRentals.map((bookRental) => ({
-        //         id: bookRental.id,
-        //         book_id: bookRental.book_id,
-        //         book: bookRental.booksRentalModelBooks,
-        //         user_id: bookRental.user_id,
-        //         user: bookRental.booksRentalModelUsers,
-        //         rental_time: bookRental.rental_time,
-        //         rented_at: bookRental.rented_at,
-        //     }))
+        const bookRentals = await db.query.booksRental.findMany()
+        return bookRentals.map(BooksRentalRepository.convert)
     }
 
-    public async delete(id: string): Promise<void> {
-        // await BooksRentalModel.destroy({ where: { id } })
+    public async delete(id: BooksRental["id"]): Promise<void> {
+        await db.delete(schema.booksRental).where(eq(schema.booksRental.id, id))
     }
 }
