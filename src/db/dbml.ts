78a98e3a6149a1ab82a44ba832a050b923edf715
David Wesley <contato.dwesley@outlook.com>
2024-03-10 22:40:52 -0300

    feat(db): add new dbml file and update database config with new sqlite URL

---
+++ src/db/dbml.ts
@@ -0,0 +1,7 @@
+// import { sqliteGenerate } from 'drizzle-dbml-generator'; // Using Postgres for this example
+// import * as schema from './schema/index.ts';
+
+// const out = './schema.dbml';
+// const relational = true;
+
+// sqliteGenerate({ schema, out, relational });
--- src/db/drizzle.config.ts
+++ src/db/drizzle.config.ts
@@ -1,13 +1,11 @@
-import { ENV } from '@/env.ts'
 import type { Config } from 'drizzle-kit'
-import path from "node:path"
 
 export default {
     schema: './schema/index.ts',
     out: './drizzle',
     driver: 'better-sqlite',
     dbCredentials: {
-        url: path.resolve(import.meta.dirname, `sqlite.${ENV.NODE_ENV}.db`)
+        url: `./sqlite.${process.env.NODE_ENV ?? "development"}.db`
     },
     verbose: true
 } satisfies Config
